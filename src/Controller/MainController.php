75ddd0881f366961dade8f3b823903568df1c1d4
ccsimmi <ffsimmi@gmail.com>
2024-05-10 17:10:03 +0100

    add twig template to controller

---
+++ src/Controller/MainController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class MainController extends AbstractController
+{
+    #[Route('/')]
+    public function homepage(): Response
+    {
+        $starShipCount = 457;
+        $myShip = [
+            'name' => 'USS LeafyCruiser (NCC-0001)',
+            'class' => 'Garden',
+            'captain' => 'Jean-Luc Pickles',
+            'status' => 'under construction'
+        ];
+
+        return $this->render('main/homepage.html.twig', [
+            'numberOfStarships' => $starShipCount,
+            'myShip' => $myShip
+        ]);
+    }
+}
